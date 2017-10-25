150216e56e8dcf37474dc2721a8a384bfa1f5036
ionJ <ion_J@outlook.com>
2017-10-25 23:59:11 +0800

    a adapter

--- app/src/main/java/com/jikexueyuan/hellonotes/MainActivity.java
+++ app/src/main/java/com/jikexueyuan/hellonotes/MainActivity.java
@@ -1,6 +1,8 @@
 package com.jikexueyuan.hellonotes;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button textbtn, imgbtn, videobtn;
     private ListView lv;
     private Intent i;
+    private MyAdapter adapter;
+    private NotesDB notesDB;
+    private SQLiteDatabase dbReader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textbtn.setOnClickListener(this);
         imgbtn.setOnClickListener(this);
         videobtn.setOnClickListener(this);
+        notesDB = new NotesDB(this);
+        dbReader = notesDB.getReadableDatabase();
     }
 
     @Override
@@ -47,6 +54,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(i);
                 break;
         }
+    }
+
+    public void selectDB() {
+        Cursor cursor = dbReader.query(NotesDB.TABLE_NAME, null, null, null, null, null, null);
+        adapter = new MyAdapter(this, cursor);
+        lv.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        selectDB();
     }
 
 
